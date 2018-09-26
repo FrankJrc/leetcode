533d630ed01dfe1ac9107be089cac18d8abbf9a4
FrankJrc <43498696+FrankJrc@users.noreply.github.com>
2018-09-26 23:50:40 +0100

    init commit

---
+++ LeetCodeSolution/LeetCodeSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodeSolution
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCodeSolution/Solutions/1_TwoSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LeetCodeSolution.Solutions
+{
+    public class Solution_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> existingNumbers = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; ++i)
+            {
+                var wanted = target - nums[i];
+                int index;
+                if (existingNumbers.TryGetValue(wanted, out index))
+                    return new[] { index, i };
+                else existingNumbers[nums[i]] = i;
+            }
+            return Array.Empty<int>();
+        }
+    }
+
+}
