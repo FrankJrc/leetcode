d4f3727f50c8a68c1e2625db009e3500f94ef940
frank <dit.frank.jia@gmail.com>
2018-09-29 13:54:54 +0100

    6 zip zap solution

--- LeetCodeSolution/LeetCodeSolution/Solutions/6_ZigZag.cs
+++ LeetCodeSolution/LeetCodeSolution/Solutions/6_ZigZag.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Text;
 namespace LeetCodeSolution.Solutions
 {
-    public class __ZigZag
+    public class ZigZag
     {
-        public __ZigZag()
+        public string Convert(string s, int numRows)
         {
+            if (numRows <= 1 || numRows >= s.Length) return s;
+
+            StringBuilder sb = new StringBuilder();
+            var denominator = numRows * 2 - 2;
+            for (int i = 0; i <= denominator / 2; ++i)
+            {
+                var step = i + i;
+                if (step == 0) step = denominator;
+                for (int j = i; j <= s.Length - 1; j += step)
+                {
+                    sb.Append(s[j]);
+                    if (step != denominator)
+                        step = denominator - step;
+                }
+            }
+            return sb.ToString();
         }
     }
 }
