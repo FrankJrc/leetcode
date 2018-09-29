70a4b22c96cc35f1cd5827e9b1f9d22adbaf6009
frank <dit.frank.jia@gmail.com>
2018-09-29 13:02:58 +0100

    leetcode 4 brutal force version

---
+++ LeetCodeSolution/LeetCodeSolution/Solutions/4_LongestPalindrome.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace LeetCodeSolution.Solutions
+{
+
+    public class LongestPalindromeSubString
+    {
+        /*
+         * 
+         * 
+         * "babad"
+         * "abcd"
+         * "a"
+         * "abcddcbaeeeabc"
+         */
+        public string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length == 1) return s;
+            int maxSoFar = 0;
+            int startIdx = -1;
+            for (int i = 0; i < s.Length-maxSoFar;++i)
+            {
+                for (int j = i + 1; j < s.Length;++j)
+                {
+                    if(s[i]==s[j])
+                    {
+                        if(IsPalindrome(s, i, j))
+                        {
+                            if(j-i+1>maxSoFar)
+                            {
+                                maxSoFar = j - i + 1;
+                                startIdx = i;
+                            }
+                        }
+                    }
+                }
+            }
+            if (startIdx == -1) return s.Substring(0,1);
+            return s.Substring(startIdx, maxSoFar);
+
+        }
+        public bool IsPalindrome(string s, int start, int end)
+        {
+            if (string.IsNullOrEmpty(s)) return false;
+            for (int i = start, j = end; i < j; ++i, --j )
+            {
+                if (s[i] != s[j]) return false;
+            }
+            return true;
+        }
+    }
+}
