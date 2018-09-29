dfa8b0708ae9053bdfb516d40c5fe161f4016f63
frank <dit.frank.jia@gmail.com>
2018-09-29 23:22:28 +0100

    palindrome number submit

--- LeetCodeSolution/LeetCodeSolution/Solutions/9_IsPalindromeNumber.cs
+++ LeetCodeSolution/LeetCodeSolution/Solutions/9_IsPalindromeNumber.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace LeetCodeSolution.Solutions
 {
-    public class __IsPalindromeNumber
+    public class IsPalindromeNumber
     {
-        public __IsPalindromeNumber()
+        public bool IsPalindrome(int x)
         {
+            if (x < 0) return false;
+            Stack<int> s = new Stack<int>();
+            Queue<int> q = new Queue<int>();
+
+            while(x!=0)
+            {
+                var lastDigit = x % 10;
+                s.Push(lastDigit);
+                q.Enqueue(lastDigit);
+                x /= 10;
+            }
+
+            while(s.Count>0)
+            {
+                if (s.Pop() != q.Dequeue()) return false;
+            }
+            return true;
         }
     }
 }
