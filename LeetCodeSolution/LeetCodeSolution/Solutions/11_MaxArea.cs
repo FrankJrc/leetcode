af9c9aa15ef6da198791f11f412269bc22284f41
frank <dit.frank.jia@gmail.com>
2018-09-30 11:12:45 +0100

    max area

---
+++ LeetCodeSolution/LeetCodeSolution/Solutions/11_MaxArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCodeSolution.Solutions
+{
+    public class MaxArea
+    {
+        public int MaxArea(int[] height)
+        {
+            int res = 0;
+            for (int i = 0; i < height.Length; ++i)
+            {
+                for (int j = i + 1; j < height.Length;++j)
+                {
+                    var area = (j - i) * Math.Min(height[i], height[j]);
+                    if (area > res) res = area;
+                }
+            }
+
+            return res;
+        }
+    }
+}
