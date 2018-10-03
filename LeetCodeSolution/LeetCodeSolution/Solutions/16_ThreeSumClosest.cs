b031f11ee7d509addf106d263458a391149a1ce9
frank <dit.frank.jia@gmail.com>
2018-10-03 23:35:20 +0100

    letter combination

---
+++ LeetCodeSolution/LeetCodeSolution/Solutions/16_ThreeSumClosest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace LeetCodeSolution.Solutions
+{
+    public class ThreeSumClosestSln
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            int res = 0;
+            int difference = int.MaxValue;
+            for (int i = 0; i < nums.Length-2;++i)
+            {
+                for (int j = i + 1; j < nums.Length-1; j++)
+                {
+                    for (int k = j + 1; k < nums.Length; ++k)
+                    {
+                        int s = nums[i] + nums[j] + nums[k];
+                        int d = Math.Abs(s - target);
+                        if (d<difference)
+                        {
+                            res = s;
+                            difference = d;
+                        }
+                        if (difference == 0) return res;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCodeSolution/Solutions/17_LetterCombinationsOnPhone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace LeetCodeSolution.Solutions
+{
+    public class LetterCombinationsOnPhone
+    {
+        static List<List<string>> Mappings = new List<List<string>>(10);
+        public IList<string> LetterCombinations(string digits)
+        {
+            if (string.IsNullOrEmpty(digits)) return Array.Empty<string>();
+
+            Mappings.Add(null);
+            Mappings.Add(null);
+            Mappings.Add(new List<string>() { "a", "b", "c" });
+            Mappings.Add(new List<string>() { "d", "e", "f" });
+            Mappings.Add(new List<string>() { "g", "h", "i" });
+            Mappings.Add(new List<string>() { "j", "k", "l" });
+            Mappings.Add(new List<string>() { "m", "n", "o" });
+            Mappings.Add(new List<string>() { "p", "q", "s","r" });
+            Mappings.Add(new List<string>() { "t", "u", "v" }); 
+            Mappings.Add(new List<string>() { "w", "x", "y", "z" });
+
+            return Solve(digits);
+
+
+        }
+
+        private IList<string> Solve(string s)
+        {
+            if (s.Length == 1)
+                return Mappings[s[0] - '0'].ToList();
+
+
+            var partialRes = Solve(s.Substring(1, s.Length - 1));
+            partialRes.Select(r => "a" + r);
+
+            return Mappings[s[0] - '0'].SelectMany(tmp => partialRes.Select(r => tmp + r)).ToList();
+        }
+    }
+}
