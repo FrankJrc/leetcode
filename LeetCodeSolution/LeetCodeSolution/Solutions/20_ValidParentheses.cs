9da965872bc8c4ef12930c4457a23559bd7b883d
frank <dit.frank.jia@gmail.com>
2018-10-04 22:48:44 +0100

    valid paraentheses

---
+++ LeetCodeSolution/LeetCodeSolution/Solutions/20_ValidParentheses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LeetCodeSolution.Solutions
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length % 2 == 1)
+                return false;
+            Stack<char> parentheses = new Stack<char>();
+            for (int i = 0; i < s.Length;++i)
+            {
+                char c = s[i];
+                if (c == '(' || c == '{' || c == '[')
+                    parentheses.Push(c);
+                else if (c == ')')
+                {
+                    if (parentheses.Count==0 || parentheses.Pop() != '(')
+                        return false;
+                }
+                else if (c == ']')
+                {
+                    if (parentheses.Count == 0 || parentheses.Pop() != '[')
+                        return false;
+                }
+                else
+                {
+                    if (parentheses.Count == 0 || parentheses.Pop() != '{')
+                        return false;
+                }
+
+            }
+            return parentheses.Count == 0;
+        }
+    }
+}
