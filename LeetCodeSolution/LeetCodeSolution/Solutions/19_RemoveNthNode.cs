2f1cbfd62921836d7cc2548078897fbb6f722eff
frank <dit.frank.jia@gmail.com>
2018-10-06 23:57:14 +0100

    remove nth node

---
+++ LeetCodeSolution/LeetCodeSolution/Solutions/19_RemoveNthNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LeetCodeSolution.Solutions
+{
+    public class RemoveNthNode
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head == null)
+                return null;
+
+            List<ListNode> nodes = new List<ListNode>();
+            while(head!=null)
+            {
+                nodes.Add(head);
+                head = head.next;
+            }
+            if(n==nodes.Count)
+            {
+                if (nodes.Count == 1) return null;
+                return nodes[1];
+            }
+
+            if(n==1)
+            {
+                nodes[nodes.Count - 2].next = null;
+                return nodes[0];
+            }
+
+            nodes[nodes.Count - n - 1].next = nodes[nodes.Count - n + 1];
+            return nodes[0];
+        }
+    }
+}
