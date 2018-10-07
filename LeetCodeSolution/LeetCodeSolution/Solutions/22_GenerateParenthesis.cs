c7e96d249d054fbb86fe256b5ad483721eb92ce0
frank <dit.frank.jia@gmail.com>
2018-10-07 11:37:44 +0100

    22 generate parenthesis

---
+++ LeetCodeSolution/LeetCodeSolution/Solutions/22_GenerateParenthesis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LeetCodeSolution.Solutions
+{
+    public class GenerateParenthesisSln
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            return Solve(n, n, "", new List<string>());
+        }
+
+        private IList<string> Solve(int left, int right, string s, List<string> res)
+        {
+            if(left==0)
+            {
+                while(right>0)
+                {
+                    s = s + ")";
+                    right--;
+                }
+                res.Add(s);
+                return res;
+            }
+
+            Solve(left - 1, right, s + "(", res);
+
+            if (right > left) Solve(left, right - 1, s + ")", res);
+
+            return res;
+
+        }
+    }
+}
